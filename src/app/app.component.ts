742367ffa46eb3c57ff6e59862eab34a6f444dcd
sudhanva <sudhanva.sa@gmail.com>
2019-04-05 17:34:32 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('video')
+  public video: ElementRef;
+
+  @ViewChild('canvas')
+  public canvas: ElementRef;
+  public captures: Array<any>;
+
+  public constructor() {
+    this.captures = new Array<any>();
+  }
+
+  public ngAfterViewInit() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.video.nativeElement.srcObject = stream;
+        this.video.nativeElement.play();
+      });
+    }
+  }
+
+  public capture() {
+    const context = this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0, 640, 480);
+    this.captures.push(this.canvas.nativeElement.toDataURL('image/png'));
+  }
+
+}
